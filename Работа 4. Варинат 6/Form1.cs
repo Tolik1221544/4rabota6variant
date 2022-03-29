7a1ae421d8ad4959e87be13637f34a9fca95c555
”Tolik1221544” <chervinskiy_anatoliy@mail.ru>
2022-03-29 21:16:58 +0300

    Initial commit

---
+++ Работа 4. Варинат 6/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Работа_4.Варинат_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Считывание начальных данных
+            double x0 = Convert.ToDouble(textBox1.Text);
+            double xk = Convert.ToDouble(textBox2.Text);
+            double dx = Convert.ToDouble(textBox3.Text);
+            double b = Convert.ToDouble(textBox4.Text);
+            textBox5.Text = "Работу выполнил ст. Червинский А.Ю." + Environment.NewLine;
+            // Цикл для табулирования функции
+            double x = x0;
+            while (x >= (xk + dx / 2))
+            {
+                double y = Math.Pow(x,2) + Math.Tan(5*x + b/x);
+                textBox5.Text += "x=" + Convert.ToString(x) +
+                    "; y=" + Convert.ToString(y) + 
+                    Environment.NewLine;
+                x = x + dx;
+            }
+        }
+    }
+}
---
+++ Работа 4. Варинат 6/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Работа 4. Варинат 6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Работа 4. Варинат 6")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a013a824-a8d1-42a2-899e-419af5d53738")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
